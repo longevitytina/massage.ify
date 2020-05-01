7012c5477b1118620cd8d1f60d31f91121f9271f
Tina <longevity.tina@gmail.com>
2020-05-01 14:24:12 -0700

    Initializes home route

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+
+# Add the following import
+from django.http import HttpResponse
+
+# Define the home view
+
+
+def home(request):
+    return HttpResponse('<h1>Massage.ify Home</h1>')
