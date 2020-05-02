6968d147e5310c5d3b37addfb8b09b64d5e6cdd8
Tina <longevity.tina@gmail.com>
2020-05-02 15:42:45 -0700

    Adds description field to technique model

--- main_app/models.py
+++ main_app/models.py
@@ -21,7 +21,8 @@ def get_absolute_url(self):
 
 class Technique(models.Model):
     name = models.CharField(max_length=100)
-    instructions = models.CharField(max_length=500)
+    description = models.CharField(max_length=1000)
+    instructions = models.CharField(max_length=1000)
     image_url = models.URLField()
     video_url = models.URLField()
 
