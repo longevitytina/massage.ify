a8fe5c198bc1950eacb9dd19d2bfab27d947620c
Tina <longevity.tina@gmail.com>
2020-05-03 16:07:42 -0700

    fixes pw error

--- main_app/scripts/reset_database.sh
+++ main_app/scripts/reset_database.sh
@@ -3,6 +3,6 @@ dropdb massageify
 createdb massageify
 python ../manage.py makemigrations
 python ../manage.py migrate
-echo "from django.contrib.auth.models import User; User.objects.create_superuser('tina', 'tina@email.com', '1234 ')" | python ../manage.py shell
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('tina', 'tina@email.com', '1234')" | python ../manage.py shell
 python ../manage.py loaddata profiles
 python ../manage.py loaddata techniques
