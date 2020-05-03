c2c889dc199d8548351c80aaadc8739915b074e8
Tina <longevity.tina@gmail.com>
2020-05-03 16:00:39 -0700

    Adds seed data

--- main_app/scripts/reset_database.sh
+++ main_app/scripts/reset_database.sh
@@ -3,5 +3,6 @@ dropdb massageify
 createdb massageify
 python ../manage.py makemigrations
 python ../manage.py migrate
-python ../manage.py loaddata techniques
 echo "from django.contrib.auth.models import User; User.objects.create_superuser('tina', 'tina@email.com', '1234 ')" | python ../manage.py shell
+python ../manage.py loaddata profiles
+python ../manage.py loaddata techniques
