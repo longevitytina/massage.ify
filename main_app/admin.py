84084ffc75b711f267772f94478fce49a2af4fe7
Tina <longevity.tina@gmail.com>
2020-05-03 14:35:52 -0700

    Registers profile model

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
-from .models import Technique, Favorites
+from .models import Technique, Profile
+
+
+# class Favorites(admin.ModelAdmin):
+#     list_display = ('user', 'technique')
+
 
 # Register your models here.
 admin.site.register(Technique)
-admin.site.register(Favorites)
+admin.site.register(Profile)
+
+
+admin.site.site_header = "Massage.ify Admin"
