3b60cca58792120efa80ef333eaa0ed60cc6ba84
Tina <longevity.tina@gmail.com>
2020-05-02 21:22:02 -0700

    Registers favorites model

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Technique
+from .models import Technique, Favorites
 
 # Register your models here.
 admin.site.register(Technique)
+admin.site.register(Favorites)
