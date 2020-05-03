8dc65b7f843bb956078654166b23c7f2a7403074
Tina <longevity.tina@gmail.com>
2020-05-03 15:38:19 -0700

    Imports technique

--- main_app/models/playlist.py
+++ main_app/models/playlist.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from datetime import date
 from django.urls import reverse
+from .technique import Technique
 
 
 class Playlist(models.Model):
