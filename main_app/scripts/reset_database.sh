170b73d7035f8f8c771a514e6647fc36d6cfc595
Tina <longevity.tina@gmail.com>
2020-05-03 14:50:56 -0700

    Fixes script

--- main_app/scripts/reset_database.sh
+++ main_app/scripts/reset_database.sh
@@ -4,3 +4,4 @@ createdb massageify
 python ../manage.py makemigrations
 python ../manage.py migrate
 python ../manage.py loaddata techniques
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('tina', 'tina@email.com', '1234 ')" | python ../manage.py shell
