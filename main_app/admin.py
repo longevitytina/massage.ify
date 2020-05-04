6ec7868e42c1654df5236d8ae8bcd6461f3a29f3
Tina <longevity.tina@gmail.com>
2020-05-04 10:51:51 -0700

    Admin panel display username

--- main_app/admin.py
+++ main_app/admin.py
@@ -2,16 +2,20 @@ from django.contrib import admin
 from .models import Technique, Profile, Playlist, PlaylistTechnique, Following
 
 
-# class Favorites(admin.ModelAdmin):
-#     list_display = ('user', 'technique')
+class ProfileA(admin.ModelAdmin):
+    list_display = ('user',)
+
+
+class FollowingA(admin.ModelAdmin):
+    list_display = ('from_user', 'to_user')
 
 
 # Register your models here.
 admin.site.register(Technique)
-admin.site.register(Profile)
+admin.site.register(Profile, ProfileA)
 admin.site.register(Playlist)
 admin.site.register(PlaylistTechnique)
-admin.site.register(Following)
+admin.site.register(Following, FollowingA)
 
 
 admin.site.site_header = "Massage.ify Admin"
