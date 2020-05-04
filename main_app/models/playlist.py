bf70d090ed99805b5536a313e163ad17e30785e4
Tina <longevity.tina@gmail.com>
2020-05-03 17:02:19 -0700

    Updates playlist model to profile -user

--- main_app/models/playlist.py
+++ main_app/models/playlist.py
@@ -7,7 +7,7 @@ from .technique import Technique
 
 class Playlist(models.Model):
     name = models.CharField(max_length=100)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey('Profile', on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
